c9dfb5e8edc3c1518e0dc8b82490ebc9c1a6e5f8
johann219 <147496124+johann219@users.noreply.github.com>
2023-12-04 13:12:29 +0300

    Add button to delete and redraw grid with given squares per side

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 const container = document.querySelector(".container");
 
-let input = 16;
+function drawGrid (side = 16) {
+  for (let i = 0; i < side; ++i) {
+    const row = document.createElement('div');
+    row.className = "row";
 
-for (let i = 0; i < input; ++i) {
-  const row = document.createElement('div');
-  row.className = "row";
+    for (let j = 0; j < side; ++j) {
+      const rowElement = document.createElement('div');
+      rowElement.className = "box";
+      if (j === side - 1) {rowElement.classList.add("last-column");}
+      if (i === side - 1) {rowElement.classList.add("last-row");}
+      row.appendChild(rowElement);
+    }
 
-  for (let j = 0; j < input; ++j) {
-    const rowElement = document.createElement('div');
-    rowElement.className = "box";
-    if (j === input - 1) {rowElement.classList.add("last-column");}
-    if (i === input - 1) {rowElement.classList.add("last-row");}
-    row.appendChild(rowElement);
+    container.appendChild(row);
   }
-
-  container.appendChild(row);
 }
-
 document.body.addEventListener ('mouseover', (e) => {
   let box = e.target.closest('.box');
   if(!box) { return; }
   box.style.backgroundColor = "lightblue";
 });
+
+
+const btn = document.querySelector(".btn");
+btn.addEventListener('click', () => {
+  let input = prompt("What number of squares per side do you want?", "16");
+  container.replaceChildren();
+  drawGrid(input);
+});
+
+drawGrid();
